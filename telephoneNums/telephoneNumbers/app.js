56f97458a29fd5b976f4d1eddb187a97d1d29864
Warriorroq <Warriorroq@gmail.com>
2021-04-11 15:39:56 +0300

    Create node exsample for task

--- telephoneNums/telephoneNumbers/app.js
+++ telephoneNums/telephoneNumbers/app.js
@@ -16,4 +16,42 @@ for (var i = 0; i < N; i++) {
 }
 console.log(arraySet.size)
 console.log(arraySet)
+
+//<------------Second turn------------------->
+
+class Tree {
+    constructor(telephoneNum) {
+        this.nodes = []
+        this.value = telephoneNum[0]
+    }
+
+    addNode(telephoneNumber) {
+        if (telephoneNumber.length != 0) {
+            for (var node of this.nodes) {
+                if (node.value == telephoneNumber[0]) {
+                    node.addNode(telephoneNumber.substr(1))
+                    return;
+                }
+            }
+            this.createNode(telephoneNumber)
+        }
+    }
+    createNode(telephoneNumber) {
+        var newNode = new Tree(telephoneNumber[0])
+        newNode.addNode(telephoneNumber.substr(1))
+        this.nodes.push(newNode)
+    }
+    nodeSize() {
+        var size = 1
+        for (var node of this.nodes)
+            size += node.nodeSize()
+        return size
+    }
+}
+
+N = getRandomInt(1, 15);
+var tree = new Tree(NaN)
+for (var i = 0; i < N; i++)
+    tree.addNode(getRandomInt(1000000000, 9999999999).toString())
+console.log(tree.nodeSize() - 1)
 while (true);
